b3517badd860139c90cd6dbfe6f1e98817cd7635
ahmtcnsvr <ahmettcansever@gmail.com>
2021-04-24 21:02:42 +0200

    update

--- aws/projects/002-milliseconds-converter/app1.py
+++ aws/projects/002-milliseconds-converter/app1.py
@@ -10,7 +10,7 @@ def convert(millisecond):
 
     minute_in_millisecond = 60*1000
     minutes = millisecond_left // minute_in_millisecond
-    millisecond_left %= minute_in_millisecond
+    millisecond_left = millisecond % minute_in_millisecond
 
     seconds = millisecond_left // 1000
 
